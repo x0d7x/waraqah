8980b53d7cbfb29e1a2e92da7f777559bcd6594b
0xWal <imwaleed@outlook.sa>
2025-08-23 03:07:42 +0300

    feat(ui): use `wails` as the ui framework :sparkles:

---
+++ ui/go.mod
@@ -0,0 +1,5 @@
+module github.com/0xdevar/waraqah/ui
+
+go 1.24
+
+require github.com/wailsapp/wails/v2 v2.10.2
---
+++ ui/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/0xdevar/waraqah/ui/ui"
+)
+
+func main() {
+	ui.StartApp()
+}
